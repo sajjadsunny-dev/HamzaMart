30c0aac8a74cbe7c939d57051bf242d4c5d6251e
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-02-15 22:34:44 +0600

    new js file added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// ================ banner part slider ======================
+$('.banner_slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+
+});
