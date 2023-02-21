bbea1adb0bc06f7a188380168498537d3b2a074d
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-02-21 23:40:05 +0600

    modified some codes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// ================ Header_part nav active js ======================
+
+// Get the container element
+var btnContainer = document.getElementById("header_part");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav_btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
 // ================ banner part slider ======================
 $('.banner_slide').slick({
     dots: true,
@@ -23,48 +40,49 @@ $('.product_slider').slick({
     nextArrow: '<i class=" next fa-solid fa-angle-right"></i>',
   });
 
-//   var countDownDate = new Date("Feb 27, 2023 00:00:00").getTime();
-//   var x = setInterval(function(){
-//     var now = new Date().fetTime();
-//     var distance = countDownDate - now;
-
-//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//     document.getElementById("days").innerHTML = days;
-//     document.getElementById("hours").innerHTML = hours;
-//     document.getElementById("minutes").innerHTML = minutes;
-//     document.getElementById("seconds").innerHTML = seconds;
-
-//   },1000);
-
-  // Set the date we're counting down to
+// ================flashsall countdown js==============
 var countDownDate = new Date("Mar 15, 2023 00:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("days").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-  document.getElementById("hours").innerHTML = hours + "h "
-  + minutes + "m " + seconds + "s ";
-  document.getElementById("minutes").innerHTML = minutes + "m " + seconds + "s ";
+  document.getElementById("days").innerHTML = days + "d ";
+  document.getElementById("hours").innerHTML = hours + "h ";
+  document.getElementById("minutes").innerHTML = minutes + "m ";
   document.getElementById("seconds").innerHTML = seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+// ================upcomming countdown js==============
+var countDownDate1 = new Date("Mar 8, 2023 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate1 - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days1").innerHTML = days + "d ";
+  document.getElementById("hours1").innerHTML = hours + "h ";
+  document.getElementById("minutes1").innerHTML = minutes + "m ";
+  document.getElementById("seconds1").innerHTML = seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
